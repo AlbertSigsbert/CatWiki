a2cd0b13317e7320b92a7cfeb80f4af12cd705f2
Albert <ishengomaalbert@gmail.com>
2023-01-31 21:12:39 +0300

    Add conditional rendering to fix a bug in prod

--- components/Details/Hero.js
+++ components/Details/Hero.js
@@ -1,59 +1,60 @@
 import Image from "next/image";
 import StatRating from "./StatRating";
 
-function Hero({cat, photos}) {
+function Hero({ cat, photos }) {
   const photo = photos[0];
-
 
   const stats = [
     {
       attribute: "adaptability",
-      rating: `${cat.adaptability}`
+      rating: `${cat.adaptability}`,
     },
     {
       attribute: "affection Level",
-      rating: `${cat.affection_level}`
+      rating: `${cat.affection_level}`,
     },
     {
       attribute: "child Friendly",
-      rating: `${cat.child_friendly}`
+      rating: `${cat.child_friendly}`,
     },
     {
       attribute: "grooming",
-      rating: `${cat.grooming}`
+      rating: `${cat.grooming}`,
     },
     {
       attribute: "health Issues",
-      rating: `${cat.health_issues}`
+      rating: `${cat.health_issues}`,
     },
     {
       attribute: "social Needs",
-      rating: `${cat.social_needs}`
+      rating: `${cat.social_needs}`,
     },
     {
       attribute: "stranger Friendly",
-      rating: `${cat.stranger_friendly}`
+      rating: `${cat.stranger_friendly}`,
     },
   ];
   return (
     <section className="my-8 grid grid-cols-1 lg:grid-cols-3 gap-8">
-      <div className="relative w-64 h-64 xl:w-96 xl:h-96 place-self-center lg:place-self-start">
-        <Image
-          priority
-          src={photo.url}
-          alt={cat.name}
-          fill
-          className="object-cover rounded-3xl z-10"
-        />
-        <div className="absolute bg-[#DEC68B] h-48 xl:h-72 rounded-[14px] w-16 z-0 top-0 -left-4 translate-y-12"></div>
-      </div>
+      {photo && (
+        <div className="relative w-64 h-64 xl:w-96 xl:h-96 place-self-center lg:place-self-start">
+          <Image
+            priority
+            src={photo.url}
+            alt={cat.name}
+            fill
+            className="object-cover rounded-3xl z-10"
+          />
+          <div className="absolute bg-[#DEC68B] h-48 xl:h-72 rounded-[14px] w-16 z-0 top-0 -left-4 translate-y-12"></div>
+        </div>
+      )}
 
       <div className="lg:col-span-2 md:place-self-center  font-montserrat">
         <h1 className="text-2xl md:text-4xl font-semibold text-chocolate">
           {cat.name}
         </h1>
         <p className="max-w-xl mt-8 font-medium text-base md:text-lg">
-         {cat.description}
+          {cat.description}
         </p>
         <p className="font-medium text-xs md:text-base my-8">
           <strong className="font-bold">Temperament:</strong> {cat.temperament}
@@ -62,7 +63,8 @@ function Hero({cat, photos}) {
           <strong className="font-bold">Origin:</strong> {cat.origin}
         </p>
         <p className="font-medium text-xs md:text-base mb-8">
-          <strong className="font-bold">Life Span:</strong> {cat.life_span} years
+          <strong className="font-bold">Life Span:</strong> {cat.life_span}{" "}
+          years
         </p>
 
         {stats &&
